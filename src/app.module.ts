3533a0870bda2b0a1e10acd1a0460fd879bfbdba
Ayslan Fernandes <lalanfernandes@gmail.com>
2022-09-19 11:47:29 -0300

    heroku

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://totalzero:aGGRBthBwoJfzGkq@cluster0.xxjxw1p.mongodb.net/test',
+    ),
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
